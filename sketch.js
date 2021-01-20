69dd4b4415c9ed10ffe27cbd31403405707a405d
sv9120 <77186409+sv9120@users.noreply.github.com>
2021-01-20 19:21:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var a,b;
+
+function setup() {
+  createCanvas(800,400);
+  a=createSprite(600, 200, 50, 50);
+  b=createSprite(400, 200, 50, 50);
+
+a.shapeColor = "green";
+b.shapeColor = "green";
+
+}
+}
+
+function draw() {
+  background(255,255,255);  
+
+  a.x = World.mouseX;
+  a.y = World.mouseY;
+
+  if(a.x-b.x<a.width/2+b.width/2 && b.x-a.x<a.width/2+b.width/2){
+
+a.shapeColor = "red";
+b.shapecolor = "red";
+
+  }
+  else{
+
+    a.shapeColor = "green";
+    b.shapecolor = "green";
+    
+
+  }
+  if(a.y-b.y<a.height/2+b.height/2 && b.y-a.y<a.height/2+b.height/2){
+
+    a.shapeColor = "red";
+    b.shapecolor = "red"
+    
+      }
+      else{
+    
+        a.shapeColor = "green";
+        b.shapecolor = "green";
+        
+    
+      }
+    
+
+  
+
+
+  drawSprites();
+}
